13754bc5a6d08224152102c5d45d3f6d310b6260
Jonathan72797 <148806314+Jonathan72797@users.noreply.github.com>
2023-12-05 16:53:17 -0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,19 @@
+*because of reddits new rate limit, you will have to wait between comments*
+
+
+First, import praw and time.
+
+In order to use this bot, you will need a client id, client secret, username and password to satisfy Reddit's API credentials.
+This can be found after creating an app at reddit.cim/prefs/aoos and your user name and password are the login credentials of your account.
+
+For this project, I am using a throwaway account with the username RedditFinalBot with the password Theomnivision1. Feel free to use it
+
+Enter the keytword, this will be the word that this bot will search for to comment in.
+Next, enter the reply_message. This is the reply you want the bot to comment. and then you will enter the specific subreddit you wish to search.
+
+You will then create a reddit instance from the bot class.
+then you will use a monitor_comments function which will search reddit for your keyword.
+
+Then you will use a try-except block which will post the actual comment followed by utilizing async to put the bot in sleep for 60 seconds before posting another comment.
+then, you use process_comment to reply to the comment.
+
---
+++ main (2).py
@@ -0,0 +1,69 @@
+import praw
+import time
+
+
+
+
+# Reddit API credentials
+client_id = 'zhqVoye8iz6Exe9Bk_n8pw'
+client_secret = 'mCJ98inv4C-FvSfZ7r-N4HHrq6on7w'
+username = 'RedditBotFinal'
+password = 'Theomnivision1'
+user_agent = '<RedditBot:1.0>'
+
+
+
+
+# Keyword to monitor
+keyword = 'Trump'
+
+
+
+
+# Reply message
+reply_message = 'This is about Trump.'
+
+
+
+# Subreddit to monitor
+subreddit_name = 'Politics'
+
+
+
+# Create a Reddit instance
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    username=username,
+    password=password,
+    user_agent=user_agent
+)
+
+
+
+def monitor_comments():
+    subreddit = reddit.subreddit(subreddit_name)
+
+    print(f"Monitoring comments in /r/{subreddit_name}. Press Ctrl+C to exit.")
+
+    while True:
+        try:
+            for comment in subreddit.stream.comments(skip_existing=True):
+                process_comment(comment)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            time.sleep(60)  # Wait for a minute before retrying
+
+
+
+def process_comment(comment):
+  if keyword.lower() in comment.body.lower():
+      # Reply to the comment
+      comment.reply(reply_message)
+      print(f"Replied to comment {comment.id} in /r/{subreddit_name}.")
+
+
+
+
+if __name__ == "__main__":
+  monitor_comments()
